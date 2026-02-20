a62cf728f67bf45914cc46a0646ed988808609b3
singhh-gauravv <gauravsunil2005@gmail.com>
2026-02-20 10:38:31 +0530

    Hero.jsx file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const[theme, setTheme]=useState(localStorage.getItem('theme') || 'light')
   return (
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero/>
     </div>
   )
 }
